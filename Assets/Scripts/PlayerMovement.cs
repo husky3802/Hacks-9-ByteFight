10a9585f2e7111d045140c37b9a2325390861bd6
husky3802 <nfollo7@gmail.com>
2024-02-10 00:11:31 -0500

    Added a double jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
@@ -9,20 +8,35 @@ public class PlayerMovement : MonoBehaviour
 {
 
     Rigidbody2D rigidbody2d;
+    BoxCollider2D boxcollider2d;
 
-    public float speed = 3.0f;
+    public LayerMask Ground_layermask;
+    public float speed = 7.0f;
+    public float jump = 7.0f;
+    public int maxdoublejumps = 1;
+    int doublejumps;
+    float time = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        boxcollider2d = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Time.deltaTime
 
-        
+        if (Input.GetKeyDown(KeyCode.W) && (isGrounded() || doublejumps >= 1))
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jump);
+            doublejumps--;
+            Debug.Log(doublejumps);
+        }
+
+
     }
 
 
@@ -38,16 +52,29 @@ public class PlayerMovement : MonoBehaviour
         //go right
         if (Input.GetKey(KeyCode.D))
         {
-
             rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
         }
-
 
         //stops the sliding
         if ((rigidbody2d.velocity.x < speed && rigidbody2d.velocity.x > 0) || (rigidbody2d.velocity.x > -speed && rigidbody2d.velocity.x < 0))
         {
             rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
         }
+
+        // limit the number of jumps
+        if (isGrounded() && doublejumps != maxdoublejumps)
+        {            
+            doublejumps = maxdoublejumps;
+            Debug.Log(doublejumps);
+        }
+        
     }
+
+    bool isGrounded()
+    {
+        RaycastHit2D raycasthit2d = Physics2D.BoxCast(rigidbody2d.position, boxcollider2d.bounds.size, 0f, Vector2.down, 0.3f, Ground_layermask);        
+        return raycasthit2d.collider != null;
+    }
+
 
 }
