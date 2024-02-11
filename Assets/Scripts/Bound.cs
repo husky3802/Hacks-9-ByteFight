2f3ea2fc8d56a43642d65159a599e67fb5147437
Ethan M Pham <ethanpham001@gmail.com>
2024-02-11 05:29:17 -0500

    bounds

---
+++ Assets/Scripts/Bound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Byte")
+        {
+            SceneManager.LoadSceneAsync(2);
+        } else if (other.tag == "Bug")
+        {
+            SceneManager.LoadSceneAsync(2);
+        }
+    }
+}
